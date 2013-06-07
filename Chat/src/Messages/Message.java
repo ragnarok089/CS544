8060cfb5dec19d136be97e11c28327a534188343
kbp <kbp48@drexel.edu>
2013-06-06 20:27:12 -0400

    bug fixes

--- Chat/src/Messages/Message.java
+++ Chat/src/Messages/Message.java
@@ -52,10 +52,11 @@ public class Message {
 	
 	public byte[] numToByte(int num,int numBytes){
 		String numstr=Integer.toBinaryString(num);
+		while(numstr.length()%8!=0){
+			numstr="0".concat(numstr);
+		}
 		byte[] storage=new byte[numBytes];
 		for(int i=0;i<numBytes && i<Integer.toString(num).length();i++){
-			System.out.println(num);
-			System.out.println(numstr.substring(numstr.length()-8*(i+1),numstr.length()-8*i));
 			storage[i]=Byte.decode(numstr.substring(numstr.length()-8*(i+1),numstr.length()-8*i));	
 		}
 		return storage;
