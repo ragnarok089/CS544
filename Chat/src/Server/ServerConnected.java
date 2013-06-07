bb7fe623ab3a0c1e4e35764dec93bab73fc1ceaf
kbp <kbp48@drexel.edu>
2013-06-07 19:00:02 -0400

    server fixes

--- Chat/src/Server/ServerConnected.java
+++ Chat/src/Server/ServerConnected.java
@@ -8,32 +8,10 @@ import Messages.*;
 public class ServerConnected extends ServerState{
 
 	public ServerState process(TCP tcp, Message tcpMessage, long timeEnteredState) {
-		if(tcpMessage instanceof ClientRequestInfoMessage && tcpMessage.getCorrect()){
-			Message message = null;
-			String user=((ClientRequestInfoMessage)tcpMessage).targetUsername;
-			String ip=LookupTable.lookup(user);
-			if(ip==null){
-				message=new LookupFailedMessage(12,LookupFailedMessage.minSize+Message.minSize,0,"",user);
-			}
-			else{
-				message=new ServerSendsInfoMessage(9,ServerSendsInfoMessage.minSize+Message.minSize,0,"",user,ip);
-			}
+		if(tcpMessage instanceof ServerHandShakeMessage && tcpMessage.getCorrect()){
+			Message message = new ServerAcceptMessage(4,Message.minSize,0,"",new byte[0]);
 			tcp.send(message);
-			return this;
-		}
-		else if(tcpMessage instanceof ClientRequestUpdateMessage && tcpMessage.getCorrect()){
-			Message message = null;
-			String user=((ClientRequestUpdateMessage)tcpMessage).senderUsername;
-			String ip=((ClientRequestUpdateMessage)tcpMessage).senderIP;
-			if(LookupTable.lookup(user)!=null){
-				message=new NameCollisionMessage(14,NameCollisionMessage.minSize+Message.minSize,0,"",user);
-			}
-			else{
-				LookupTable.bind(user, ip);
-				message=new ServerConfirmationUpdateMessage(7,ServerConfirmationUpdateMessage.minSize+Message.minSize,0,"",user,ip);
-			}
-			tcp.send(message);
-			return this;
+			return new ServerReady();
 		}
 		else if(tcpMessage!=null){
 			tcp.send(new ErrorMessage(13,Message.minSize,0,"",new byte[0]));
